736a2bce80916bc851f3dcf0a2b6d7cd7f11a6a8
Samundra9999 <149924828+Samundra9999@users.noreply.github.com>
2025-09-16 22:23:21 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle
 import streamlit as st
 import sklearn
@@ -142,4 +141,5 @@ with tab2:
 
             fig.update_layout(title="Run Distribution")
             st.plotly_chart(fig)      
+
 
